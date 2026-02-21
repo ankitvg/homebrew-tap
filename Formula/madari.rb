fb333c929d8f36648330271affee5ed3cc7a8e50
Ankit Gupta <ankit6791@gmail.com>
2026-02-21 15:06:25 -0500

    Use public tap release assets for madari formula

--- Formula/madari.rb
+++ Formula/madari.rb
@@ -5,20 +5,20 @@ class Madari < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ankitvg/madari/releases/download/v#{version}/madari-darwin-arm64.tar.gz"
+      url "https://github.com/ankitvg/homebrew-tap/releases/download/madari-v#{version}/madari-darwin-arm64.tar.gz"
       sha256 "08bb19dce5a089de713b644e5d49e4fad3e29623e8ce3e2305d27c0ab5147902"
     else
-      url "https://github.com/ankitvg/madari/releases/download/v#{version}/madari-darwin-amd64.tar.gz"
+      url "https://github.com/ankitvg/homebrew-tap/releases/download/madari-v#{version}/madari-darwin-amd64.tar.gz"
       sha256 "e0453a7f8b92464167398d36bc851838d82be7f8e80424116565d425977bb5df"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/ankitvg/madari/releases/download/v#{version}/madari-linux-arm64.tar.gz"
+      url "https://github.com/ankitvg/homebrew-tap/releases/download/madari-v#{version}/madari-linux-arm64.tar.gz"
       sha256 "2e73f6d7b5517f0bad6b02cd94c23f64b58e9200a5f7edfb87c98165e37534de"
     else
-      url "https://github.com/ankitvg/madari/releases/download/v#{version}/madari-linux-amd64.tar.gz"
+      url "https://github.com/ankitvg/homebrew-tap/releases/download/madari-v#{version}/madari-linux-amd64.tar.gz"
       sha256 "0234625073dbf831bc0e1df765dba1b7238d449666e713e80da68429bccdb8a9"
     end
   end
