47a4c5f51e2e51853b2b8f4d7bffb1c91c0cbb8d
Ankit Gupta <ankit6791@gmail.com>
2026-06-20 17:29:03 -0400

    Bump stew formula to 0.1.2

--- Formula/stew.rb
+++ Formula/stew.rb
@@ -1,30 +1,30 @@
 class Stew < Formula
   desc "CLI for append-only markdown project memory ledgers"
   homepage "https://github.com/ankitvg/stew"
-  version "0.1.1"
+  version "0.1.2"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
       url "https://github.com/ankitvg/stew/releases/download/v#{version}/stew_v#{version}_darwin_arm64.tar.gz"
-      sha256 "5ee63baba6c35edd08cd783781b3bc4ea525722ae5c1a705bc98190fcd309fc1"
+      sha256 "42924807b280e36a630071406422624d743e677c49adc6a678f2baaf18fdd262"
     end
 
     on_intel do
       url "https://github.com/ankitvg/stew/releases/download/v#{version}/stew_v#{version}_darwin_amd64.tar.gz"
-      sha256 "bb601b7a06f980773333371d6821bf3ae01bb244bf9be450eee0741efe550928"
+      sha256 "563e28e75d96c4846c74bd7e0de21970888d73abd5fc04dd97c91b846a14c0ea"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/ankitvg/stew/releases/download/v#{version}/stew_v#{version}_linux_arm64.tar.gz"
-      sha256 "f28c429e882918371586c4d768c861e0907d72a518bdba9ec5ff9b5067438295"
+      sha256 "6db69a87bb4f32d486f5185b38d205e8c98a02b7df23f3a98949ae17ee57c865"
     end
 
     on_intel do
       url "https://github.com/ankitvg/stew/releases/download/v#{version}/stew_v#{version}_linux_amd64.tar.gz"
-      sha256 "66fdb0884adf605064ad134aa37b6b3ebc9cc62c3160c73191f2b116b272b6d3"
+      sha256 "d11973fac5ef3ed93f1c73f98dadb56b128c7681c5605b41b0f372a73602054d"
     end
   end
 
